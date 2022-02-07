f0a6fa861264c70e44b1d942f7ad2815d7549a97
Dora-ApplePie <dasha0000golenko@gmail.com>
2022-02-07 13:53:38 +0300

    add func salaryReducer

--- src/store/tasks.test.ts
+++ src/store/tasks.test.ts
@@ -1,4 +1,11 @@
-import {addSalary, fallingSalary, multipleSalary, divideSalary} from "./tasks";
+import {
+    addSalary,
+    fallingSalary,
+    multipleSalary,
+    divideSalary,
+    salaryReducer,
+    AddSalaryActionType, FallSalaryActionType, MultipleSalaryActionType, DivideSalaryActionType
+} from "./tasks";
 
 test("AddSalary", () => {
     //1. Тестовые данные
@@ -26,3 +33,39 @@ test("Multiply Salary", () => {
 test("Divide Salary", () => {
     expect(divideSalary(1000, 0.9)).toBe(900)
 })
+
+test("ADD SALARY of salaryReducer", ()=>{
+    const salary: number = 700;
+    const action: AddSalaryActionType = {
+        type: 'ADD-SALARY',
+        bonus: 300
+    }
+    expect(salaryReducer(salary, action)).toBe(1000)
+} )
+
+test("FALL SALARY of salaryReducer", ()=>{
+    const salary: number = 2000;
+    const action: FallSalaryActionType = {
+        type: 'FALL-SALARY',
+        minus: 300
+    }
+    expect(salaryReducer(salary, action)).toBe(1700)
+} )
+
+test("MULTIPLE SALARY of salaryReducer", ()=>{
+    const salary: number = 1000;
+    const action: MultipleSalaryActionType = {
+        type: "MULTIPLE-SALARY",
+        coefficient: 1.2
+    }
+    expect(salaryReducer(salary, action)).toBe(1200)
+} )
+
+test("DIVIDE SALARY of salaryReducer", ()=>{
+    const salary: number = 1000;
+    const action: DivideSalaryActionType = {
+        type: "DIVIDE-SALARY",
+        coefficient: 0.8
+    }
+    expect(salaryReducer(salary, action)).toBe(800)
+} )
--- src/store/tasks.ts
+++ src/store/tasks.ts
@@ -2,14 +2,51 @@ export const addSalary = (salary: number, bonus: number) => {
     return salary + bonus
 }
 
-export const  fallingSalary = (salary: number, minus: number) => {
+export const fallingSalary = (salary: number, minus: number) => {
     return salary - minus
 }
 
-export const  multipleSalary = (salary: number, coefficient: number) => {
+export const multipleSalary = (salary: number, coefficient: number) => {
     return salary * coefficient
 } // ex. 1.2, >1
 
-export const  divideSalary = (salary: number, coefficient: number) => {
+export const divideSalary = (salary: number, coefficient: number) => {
     return salary * coefficient
 } // ex. 0,9, <1
+
+
+export type AddSalaryActionType = {
+    type: 'ADD-SALARY',
+    bonus: number
+}
+export type FallSalaryActionType = {
+    type: 'FALL-SALARY',
+    minus: number
+}
+export type MultipleSalaryActionType = {
+    type: 'MULTIPLE-SALARY',
+    coefficient: number
+}
+export type DivideSalaryActionType = {
+    type: 'DIVIDE-SALARY',
+    coefficient: number
+}
+
+type ActionType = AddSalaryActionType | FallSalaryActionType | MultipleSalaryActionType
+| DivideSalaryActionType;
+
+export const salaryReducer = (salary: number, action: ActionType) => {
+    switch (action.type) {
+        case 'ADD-SALARY':
+            return salary + action.bonus
+        case "FALL-SALARY":
+            return salary - action.minus
+        case "MULTIPLE-SALARY":
+            return salary * action.coefficient
+        case "DIVIDE-SALARY":
+            return salary * action.coefficient
+        default :
+            return salary
+
+    }
+} //чистая функция, принимает стартовые состояния и возращает один и тот же результат
